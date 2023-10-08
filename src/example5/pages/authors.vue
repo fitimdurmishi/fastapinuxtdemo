b09402318d8a028429dc0536d48754055283e999
Fitim Durmishi <fitim.durmishi@bridge-technology.net>
2023-10-08 11:16:52 +0200

    WIP example5

--- src/example5/pages/authors.vue
+++ src/example5/pages/authors.vue
@@ -1,97 +1,112 @@
 <template>
-    <div>
-        <nav>
-            <ul>
-              <li>
-                <NuxtLink to="/">Home</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="/authors">Authors</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="/books">Books</NuxtLink>
-              </li>
-            </ul>
-        </nav>
+    <b-container fluid class="p-5">
+        <b-row>
+            <b-col lg="6" class="mb-2">
+                <b-form-group
+                  label="Filter"
+                  label-for="filter-input"
+                  label-cols-sm="3"
+                  label-align-sm="right"
+                  label-size="sm"
+                  class="mb-0"
+                >
+                  <b-input-group size="sm">
+                    <b-form-input
+                      id="filter-input"
+                      v-model="filter"
+                      type="search"
+                      placeholder="Type to Search"
+                    ></b-form-input>
+        
+                    <b-input-group-append>
+                      <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                    </b-input-group-append>
+                  </b-input-group>
+                </b-form-group>
+            </b-col>
+            <b-col lg="6" class="mb-2 text-right">
+                <b-button size="sm" v-on:click="createAuthor">Create Author</b-button>
+            </b-col>
+        </b-row>
 
-        <ul v-for="a in authors" v-bind:key="a.id">
-            <li>{{a.name}}</li>
-        </ul>
-    </div>
+        <b-table 
+            small 
+            hover 
+            :items="authors" 
+            :fields="fields"
+            :filter="filter"
+            :filter-included-fields="filterOn"
+            @row-clicked="myRowClickHandler"
+        >
+            <template #cell(actions)="row">
+                <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
+                    Info modal
+                </b-button>
+                </template>
+        </b-table>
+
+        
+
+        <!-- Info modal -->
+        <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
+            <pre>{{ infoModal.content }}</pre>
+        </b-modal>
+    </b-container>
 </template>
 
 <script>
     export default {
+        layout: 'navigation',
         name: 'AuthorsPage',
         data: () => ({
-                authors: [],
-            }),
-        mounted() {
-            this.getAuthors()
+            authors: [],
+            fields: [{
+                    key: "id",
+                    label: "Id"
+                },
+                {
+                    key: "name",
+                    label: "Name"
+                },
+                {
+                    key: "books",
+                    label: "Nr. of books"
+                }
+            ],
+            infoModal: {
+                id: 'info-modal',
+                title: '',
+                content: ''
+            },
+            filter: null,
+            filterOn: [],
+        }),
+        async mounted() {
+            await this.getAuthors();
         },
         methods: {
             async getAuthors() {
-            let res = await this.$axios.get('/authors');
-            this.authors = res.data;
+                let res = await this.$axios.get('/authors');
+                this.authors = res.data;
+            },
+            info(item, index, button) {
+                this.infoModal.title = `Row index: ${index}`
+                this.infoModal.content = JSON.stringify(item, null, 2)
+                this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+            },
+            resetInfoModal() {
+                this.infoModal.title = ''
+                this.infoModal.content = ''
+            },
+            createAuthor() {
+                console.log('Create Author');
+            },
+            myRowClickHandler(record, index) {
+                console.log('myRowClickHandler', record);
+
+                // this.info(record.item, record.index, $event.target)
+                this.info(record, index);
             },
         },
     }
 </script>
-
-<style>
-/* home route and active route will show in bold as it matches / and /about */
-a.nuxt-link-active {
-  font-weight: bold;
-}
-/* exact link will show the primary color for only the exact matching link */
-a.nuxt-link-exact-active {
-  color: #00c58e;
-}
-
-body {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
-    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-  margin: 0;
-}
-
-main {
-  margin: 0 auto;
-  padding: 0 1rem;
-  margin-top: 100px;
-  max-width: 1280px;
-  text-align: center;
-}
-img {
-  margin-bottom: 1rem;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-li {
-  margin: 0 0.5rem;
-  padding: 0.25rem;
-  font-size: 1.2rem;
-}
-
-nav {
-  padding: 0 1rem;
-}
-
-a,
-a:visited {
-  text-decoration: none;
-  color: inherit;
-}
-
-a:hover {
-  color: #00c58e;
-}
-
-
-</style>
--- src/example5/pages/books.vue
+++ src/example5/pages/books.vue
@@ -1,19 +1,5 @@
 <template>
     <div>
-        <nav>
-            <ul>
-              <li>
-                <NuxtLink to="/">Home</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="/authors">Authors</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="/books">Books</NuxtLink>
-              </li>
-            </ul>
-        </nav>
-
         <ul v-for="b in books" v-bind:key="b.id">
             <li>Title: {{b.name}}, {{b.page_numbers}} pages from author {{b.author_id}}</li>
         </ul>
@@ -22,77 +8,20 @@
 
 <script>
     export default {
-        name: 'BooksPage',
-        data: () => ({
-                books: [],
-            }),
-        mounted() {
-            this.getBooks()
-        },
-        methods: {
-            async getBooks() {
-            let res = await this.$axios.get('/books');
-            this.books = res.data;
-            console.log(this.books);
-            },
-        },
+      layout: 'navigation',
+      name: 'BooksPage',
+      data: () => ({
+              books: [],
+          }),
+      mounted() {
+          this.getBooks()
+      },
+      methods: {
+          async getBooks() {
+          let res = await this.$axios.get('/books');
+          this.books = res.data;
+          console.log(this.books);
+          },
+      },
     }
 </script>
-
-<style>
-/* home route and active route will show in bold as it matches / and /about */
-a.nuxt-link-active {
-  font-weight: bold;
-}
-/* exact link will show the primary color for only the exact matching link */
-a.nuxt-link-exact-active {
-  color: #00c58e;
-}
-
-body {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
-    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-  margin: 0;
-}
-
-main {
-  margin: 0 auto;
-  padding: 0 1rem;
-  margin-top: 100px;
-  max-width: 1280px;
-  text-align: center;
-}
-img {
-  margin-bottom: 1rem;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-li {
-  margin: 0 0.5rem;
-  padding: 0.25rem;
-  font-size: 1.2rem;
-}
-
-nav {
-  padding: 0 1rem;
-}
-
-a,
-a:visited {
-  text-decoration: none;
-  color: inherit;
-}
-
-a:hover {
-  color: #00c58e;
-}
-
-
-</style>
